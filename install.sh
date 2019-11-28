5ef84c32df4f78e233a20ccc55d9b1a1e96161ad
cynthia-rempel <cynthia.m.rempel@gmail.com>
2019-11-28 10:16:52 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,4 +15,18 @@ mv cdh_public.repo /etc/yum.repos.d/
 # install knox
 yum -y localinstall knox-1.3.0.7.0.3.0-79.noarch.rpm
 
+echo "installing apacheds"
+
+# get and install the gpgkey
+wget https://www.apache.org/dist/directory/apacheds/dist/2.0.0.AM25/apacheds-2.0.0.AM25-x86_64.rpm.asc
+rpm --import apacheds-2.0.0.AM25-x86_64.rpm.asc
+
+# download apacheds
+wget https://apache.osuosl.org/directory/apacheds/dist/2.0.0.AM25/apacheds-2.0.0.AM25-x86_64.rpm
+
+# install apacheds
+yum -y localinstall apacheds-2.0.0.AM25-x86_64.rpm
+
+
+
 
