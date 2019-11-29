b78d6b8b60a825865e51f381e16c21b46b8019de
cynthia-rempel <cynthia.m.rempel@gmail.com>
2019-11-28 16:07:42 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,11 @@ curl -O https://archive.cloudera.com/cdh7/7.0.3.0/redhat7/yum/RPM-GPG-KEY/RPM-GP
 rpm --import RPM-GPG-KEY-Jenkins
 
 # download knox
+# kick-back and drink some coffee/tea/etc. this will take a while
 curl -O https://archive.cloudera.com/cdh7/7.0.3.0/redhat7/yum/knox/knox-1.3.0.7.0.3.0-79.noarch.rpm
 
 # increase the probability knox can be updated
-wget https://archive.cloudera.com/cdh7/7.0.3.0/redhat7/yum/cdh_public.repo
+curl -O https://archive.cloudera.com/cdh7/7.0.3.0/redhat7/yum/cdh_public.repo
 mv cdh_public.repo /etc/yum.repos.d/
 
 # install knox
