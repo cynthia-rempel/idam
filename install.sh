57f8771d82a6249e39694e2060c86a7c52f2c1a6
cynthia-rempel <cynthia.m.rempel@gmail.com>
2019-11-30 12:03:50 -0800

    Update install.sh
    Pull apache fortress into it's own module

--- install.sh
+++ install.sh
@@ -47,34 +47,7 @@ mv /usr/local/keycloak-8.0.0 /usr/local/keycloak
 useradd keycloak -r -d /usr/local/keycloak
 chown -R keycloak:keycloak /usr/local/keycloak
 
-# install fortress
-# install tomcat
-yum -y install tomcat
-
-# download the stuff
-wget https://apache.osuosl.org/directory/fortress/dist/2.0.3/fortress-core-2.0.3.jar
-wget https://apache.osuosl.org/directory/fortress/dist/2.0.3/fortress-realm-impl-2.0.3.jar
-wget https://apache.osuosl.org/directory/fortress/dist/2.0.3/fortress-realm-proxy-2.0.3.jar
-wget https://apache.osuosl.org/directory/fortress/dist/2.0.3/fortress-rest-2.0.3.war
-wget https://apache.osuosl.org/directory/fortress/dist/2.0.3/fortress-web-2.0.3.war
-
-# fortress realm and tomcat
-# https://github.com/apache/directory-fortress-core/blob/master/README-QUICKSTART-APACHEDS.md 
-# Step 5
-
-# put the jars where they go
-mv fortress-realm-proxy-2.0.3.jar /usr/share/tomcat/lib/
-mv fortress-web-2.0.3.war /usr/share/tomcat/webapps/
-mv fortress-rest-2.0.3.war /usr/share/tomcat/webapps/
-
-# Step 6
-# patch /etc/tomcat/tomcat-users.xml
-# with
-# 
-# <role rolename="manager-script"/>
-# <role rolename="manager-gui"/>
-# <user username="tcmanager" password="m@nager123" roles="manager-script"/>
-# <user username="tcmanagergui" password="m@nager123" roles="manager-gui"/>
+./apache-fortress/apache-fortress-install.sh
 
 # install phpldapadmin
 # get the package installed
