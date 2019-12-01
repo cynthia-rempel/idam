f3225f5206a31af12a2b967fdb23ad8eb0271f9a
cynthia-rempel <cynthia.m.rempel@gmail.com>
2019-12-01 15:44:56 -0800

    Update apache-fortress-rpmbuild.sh

--- apache-fortress/apache-fortress-rpmbuild.sh
+++ apache-fortress/apache-fortress-rpmbuild.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cd rpmbuild/SOURCES
-curl -Ol https://codeload.github.com/apache/directory-project/archive/zip/master
+curl -Ol https://codeload.github.com/apache/directory-project/zip/master
 mv master directory-project-master.zip
-curl -Ol https://codeload.github.com/apache/directory-fortress-core/archive/zip/master
+curl -Ol https://codeload.github.com/apache/directory-fortress-core/zip/master
 mv master directory-fortress-core-master.zip
-curl -Ol https://codeload.github.com/apache/directory-fortress-realm/archive/zip/master
+curl -Ol https://codeload.github.com/apache/directory-fortress-realm/zip/master
 mv master directory-fortress-realm-master.zip
-curl -Ol https://codeload.github.com/apache/directory-fortress-commander/archive/zip/master
+curl -Ol https://codeload.github.com/apache/directory-fortress-commander/zip/master
 mv master directory-fortress-commander-master.zip
-curl -Ol https://codeload.github.com/apache/directory-fortress-enmasse/archive/zip/master
+curl -Ol https://codeload.github.com/apache/directory-fortress-enmasse/zip/master
 mv master directory-fortress-enmasse-master.zip
 cd ../..
 rpmbuild -bs fortress.spec
