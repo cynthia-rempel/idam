1dbc423f4c3f97ebcc51ba607e0aa40dfd617a9f
cynthia-rempel <cynthia.m.rempel@gmail.com>
2019-12-01 12:58:17 -0800

    Update apache-fortress-rpmbuild.sh
    Grab the sources before running the build

--- apache-fortress/apache-fortress-rpmbuild.sh
+++ apache-fortress/apache-fortress-rpmbuild.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 cd rpmbuild/SOURCES
-wget
+wget https://github.com/apache/directory-project/archive/44.zip
+wget http://apache.osuosl.org//directory/fortress/dist/2.0.3/fortress-core-2.0.3-source-release.zip
+wget http://apache.osuosl.org//directory/fortress/dist/2.0.3/fortress-realm-2.0.3-source-release.zip
+wget http://apache.osuosl.org//directory/fortress/dist/2.0.3/fortress-rest-2.0.3-source-release.zip
+wget http://apache.osuosl.org//directory/fortress/dist/2.0.3/fortress-web-2.0.3-source-release.zip
+wget https://github.com/apache/directory-fortress-enmasse/archive/2.0.3.zip
+cd ../..
+rpmbuild -bs fortress.spec
