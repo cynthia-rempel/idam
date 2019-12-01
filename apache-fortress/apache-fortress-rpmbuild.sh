3642823dcfbbe5c2dd586eec91c3dbcde7183425
cynthia-rempel <cynthia.m.rempel@gmail.com>
2019-12-01 15:22:17 -0800

    Update apache-fortress-rpmbuild.sh
    wget not on centos-minimal

--- apache-fortress/apache-fortress-rpmbuild.sh
+++ apache-fortress/apache-fortress-rpmbuild.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cd rpmbuild/SOURCES
-wget https://github.com/apache/directory-project/archive/master.zip
+curl -Ol https://github.com/apache/directory-project/archive/master.zip
 mv master.zip directory-project-master.zip
-wget https://github.com/apache/directory-fortress-core/archive/master.zip
+curl -Ol https://github.com/apache/directory-fortress-core/archive/master.zip
 mv master.zip directory-fortress-core-master.zip
-wget https://github.com/apache/directory-fortress-realm/archive/master.zip
+curl -Ol https://github.com/apache/directory-fortress-realm/archive/master.zip
 mv master.zip directory-fortress-realm-master.zip
-wget https://github.com/apache/directory-fortress-commander/archive/master.zip
+curl -Ol https://github.com/apache/directory-fortress-commander/archive/master.zip
 mv master.zip directory-fortress-commander-master.zip
-wget https://github.com/apache/directory-fortress-enmasse/archive/master.zip
+curl -Ol https://github.com/apache/directory-fortress-enmasse/archive/master.zip
 mv master.zip directory-fortress-enmasse-master.zip
 cd ../..
 rpmbuild -bs fortress.spec
