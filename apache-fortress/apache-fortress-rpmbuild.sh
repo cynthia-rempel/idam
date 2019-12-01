5050ea7cdfcf7824aac9da19f6924ac5193f2a41
cynthia-rempel <cynthia.m.rempel@gmail.com>
2019-12-01 15:51:32 -0800

    Update apache-fortress-rpmbuild.sh

--- apache-fortress/apache-fortress-rpmbuild.sh
+++ apache-fortress/apache-fortress-rpmbuild.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cd rpmbuild/SOURCES
-curl -Ol https://codeload.github.com/apache/directory-project/zip/master
-mv master directory-project-master.zip
-curl -Ol https://codeload.github.com/apache/directory-fortress-core/zip/master
-mv master directory-fortress-core-master.zip
-curl -Ol https://codeload.github.com/apache/directory-fortress-realm/zip/master
-mv master directory-fortress-realm-master.zip
-curl -Ol https://codeload.github.com/apache/directory-fortress-commander/zip/master
-mv master directory-fortress-commander-master.zip
-curl -Ol https://codeload.github.com/apache/directory-fortress-enmasse/zip/master
-mv master directory-fortress-enmasse-master.zip
+curl -Ol https://codeload.github.com/apache/directory-project/tar.gz/master
+mv master directory-project-master.tar.gz
+curl -Ol https://codeload.github.com/apache/directory-fortress-core/tar.gz/master
+mv master directory-fortress-core-master.tar.gz
+curl -Ol https://codeload.github.com/apache/directory-fortress-realm/tar.gz/master
+mv master directory-fortress-realm-master.tar.gz
+curl -Ol https://codeload.github.com/apache/directory-fortress-commander/tar.gz/master
+mv master directory-fortress-commander-master.tar.gz
+curl -Ol https://codeload.github.com/apache/directory-fortress-enmasse/tar.gz/master
+mv master directory-fortress-enmasse-master.tar.gz
 cd ../..
 rpmbuild -bs fortress.spec
